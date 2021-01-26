9cb3760f4e1b62a1a50ad4e50ca0650e0ef417be
gLeobonacini <70539330+gLeobonacini@users.noreply.github.com>
2021-01-26 13:49:17 -0300

    Corrigindo bugs Firestore

--- app/src/main/java/com/kotlin/gamedescription/ui/AddActivity.kt
+++ app/src/main/java/com/kotlin/gamedescription/ui/AddActivity.kt
@@ -3,7 +3,6 @@ package com.kotlin.gamedescription.ui
 
 import android.app.AlertDialog
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -18,6 +17,8 @@ import com.squareup.picasso.Picasso
 import dmax.dialog.SpotsDialog
 import jp.wasabeef.picasso.transformations.CropCircleTransformation
 import kotlinx.android.synthetic.main.activity_add.*
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 
 class AddActivity : AppCompatActivity() {
@@ -27,7 +28,8 @@ class AddActivity : AppCompatActivity() {
     private lateinit var db: FirebaseFirestore
     private lateinit var cr: CollectionReference
     private val CODE_IMG = 1000
-    private var uri: Uri? = null
+    private val game: MutableMap<String,Any> = HashMap()
+    private var image = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,7 +48,7 @@ class AddActivity : AppCompatActivity() {
 
     private fun config(){
         alertDialog = SpotsDialog.Builder().setContext(this).build()
-        storage = FirebaseStorage.getInstance().getReference("game_img")
+        storage = FirebaseStorage.getInstance().getReference((System.currentTimeMillis() / 1000).toString())
         db = FirebaseFirestore.getInstance()
         cr = db.collection("games")
     }
@@ -60,8 +62,10 @@ class AddActivity : AppCompatActivity() {
     }
 
     private fun saveGame(name: String, year: String, description: String){
-        if(uri != null) {
-            sendGame(getData())
+        Toast.makeText(this, game["image"].toString(), Toast.LENGTH_LONG).show()
+        if(image != false) {
+            getData()
+            sendGame()
             finish()
         }
         else {
@@ -69,17 +73,13 @@ class AddActivity : AppCompatActivity() {
         }
     }
 
-    private fun getData(): MutableMap<String, Any>{
-        val game: MutableMap<String,Any> = HashMap()
+    private fun getData(){
         game["name"] = idAddGameName.text.toString()
-        game["image"] = uri!!
-        game["year"] = idAddGameYear.text.toString().toInt()
-        game["description"] = idAddGameDescription.text.toString().toDouble()
-        Toast.makeText(this, game["image"].toString(), Toast.LENGTH_LONG).show()
-        return game
+        game["year"] = idAddGameYear.text.toString()
+        game["description"] = idAddGameDescription.text.toString()
     }
 
-    private fun sendGame(game: MutableMap<String, Any>){
+    private fun sendGame(){
         cr.document(game["name"].toString()).set(game).addOnSuccessListener {
             Log.i("TAG","Adicionado com sucesso")
         }.addOnFailureListener {
@@ -98,7 +98,9 @@ class AddActivity : AppCompatActivity() {
                 storage!!.downloadUrl
             }.addOnCompleteListener { task ->
                 if (task.isSuccessful) {
-                    uri = task.result
+                    val uri = task.result
+                    game["image"] = uri.toString()
+                    image = true
                     //val url = uri!!.toString()
                     //    .substring(0, downloadUri.toString().indexOf("&token"))
                     Log.i("Link Direto ", uri.toString())
